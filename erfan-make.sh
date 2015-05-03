016f8cc389d06348d8dd569b2302ce43e36d3f76
erfanz <erfan.zamanian@gmail.com>
2015-05-03 11:50:49 -0400

    Fixed both make files

--- erfan-make.sh
+++ erfan-make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ./obj
-mkdir -p ./executables/ClientCentricLM
+mkdir -p ./executables/clientCentricLM
 
 g++ -c -Wno-write-strings util/utils.cpp -o obj/utils.o
 
@@ -10,11 +10,11 @@ g++ -c -Wno-write-strings util/RDMACommon.cpp -o obj/RDMACommon.o
 g++ -c -Wno-write-strings -std=c++11 util/BaseContext.cpp  -o obj/BaseContext.o
 
 
-g++ -c -Wno-write-strings -std=c++11 ClientCentericLM/ServerContext.cpp -o obj/ServerContext.o
-g++ -c -Wno-write-strings -std=c++11 ClientCentericLM/ClientContext.cpp -o obj/ClientContext.o
+g++ -c -Wno-write-strings -std=c++11 clientCentericLM/ServerContext.cpp -o obj/ServerContext.o
+g++ -c -Wno-write-strings -std=c++11 clientCentericLM/ClientContext.cpp -o obj/ClientContext.o
 
-g++ -c -Wno-write-strings -std=c++11 ClientCentericLM/ClientCentricServer.cpp -o obj/ClientCentricServer.o
-g++ obj/utils.o obj/RDMACommon.o obj/BaseContext.o obj/ServerContext.o obj/ClientCentricServer.o -libverbs -lpthread -o executables/ClientCentricLM/ClientCentricServer
+g++ -c -Wno-write-strings -std=c++11 clientCentericLM/ClientCentricServer.cpp -o obj/ClientCentricServer.o
+g++ obj/utils.o obj/RDMACommon.o obj/BaseContext.o obj/ServerContext.o obj/ClientCentricServer.o -libverbs -lpthread -o executables/clientCentricLM/ClientCentricServer
 
-g++ -c -Wno-write-strings -std=c++11 ClientCentericLM/ClientCentricClient.cpp -o obj/ClientCentricClient.o
-g++ obj/utils.o obj/RDMACommon.o obj/BaseContext.o obj/ClientContext.o obj/ClientCentricClient.o -libverbs -lpthread -o executables/ClientCentricLM/ClientCentricClient
+g++ -c -Wno-write-strings -std=c++11 clientCentericLM/ClientCentricClient.cpp -o obj/ClientCentricClient.o
+g++ obj/utils.o obj/RDMACommon.o obj/BaseContext.o obj/ClientContext.o obj/ClientCentricClient.o -libverbs -lpthread -o executables/clientCentricLM/ClientCentricClient
---
+++ yeounoh-make.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p ./obj
+mkdir -p ./executables/serverCentricLM
+
+g++ -c -Wno-write-strings util/utils.cpp -o obj/utils.o
+
+g++ -c -Wno-write-strings util/RDMACommon.cpp -o obj/RDMACommon.o
+
+g++ -c -Wno-write-strings -std=c++11 util/BaseContext.cpp  -o obj/BaseContext.o
+
+
+g++ -c -Wno-write-strings -std=c++11 serverCentricLM/ClientContext.cpp -o obj/ClientContext.o
+g++ -c -Wno-write-strings -std=c++11 serverCentricLM/ServerContext.cpp -o obj/ServerContext.o
+
+g++ -c -Wno-write-strings -std=c++11 serverCentricLM/LockServer.cpp -o obj/LockServer.o
+g++ utils.o obj/RDMACommon.o obj/BaseContext.o obj/ClientContext.o obj/ServerContext.o obj/LockServer.o -libverbs -lpthread -o executables/serverCentricLM/LockServer
+
+g++ -c -Wno-write-strings -std=c++11 serverCentricLM/LockClient.cpp -o obj/LockClient.o
+g++ utils.o obj/RDMACommon.o obj/BaseContext.o obj/ClientContext.o obj/LockClient.o -libverbs -lpthread -o executables/serverCentricLM/LockClient
